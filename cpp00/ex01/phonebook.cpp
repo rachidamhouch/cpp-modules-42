f8d437dd5090bedbb6ebdd20daf5012b20b5376a
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-14 19:09:39 +0000

    anything

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 18:56:55 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/14 18:58:14 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/14 19:09:21 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,54 @@ PhoneBook::PhoneBook()
 }
 PhoneBook::~PhoneBook()
 {
+}
+
+std::string PhoneBook::get_fname(int i)
+{
+	return (this->contact[i].get_fname());
+}
+
+std::string PhoneBook::get_lname(int i)
+{
+	return (this->contact[i].get_lname());
+}
+
+std::string PhoneBook::get_nickname(int i)
+{
+	return (this->contact[i].get_nickname());
+}
+
+std::string PhoneBook::get_number(int i)
+{
+	return (this->contact[i].get_number());
+}
+
+std::string PhoneBook::get_darkest(int i)
+{
+	return (this->contact[i].get_darkest());
+}
+
+void PhoneBook::set_fname(std::string fname, int i)
+{
+	this->contact[i].set_fname(fname);
+}
+
+void PhoneBook::set_lname(std::string lname, int i)
+{
+	this->contact[i].set_lname(lname);
+}
+
+void PhoneBook::set_nickname(std::string nickname, int i)
+{
+	this->contact[i].set_nickname(nickname);
+}
+
+void PhoneBook::set_number(std::string number, int i)
+{
+	this->contact[i].set_number(number);
+}
+
+void PhoneBook::set_darkest(std::string darkest, int i)
+{
+	this->contact[i].set_darkest(darkest);
 }
