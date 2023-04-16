058b4cd48eb28daf44edb1e3cd8d9d22621076eb
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-16 22:14:09 +0000

    anything

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -14,13 +14,15 @@ Account::Account(void)
 
 Account::~Account(void)
 {
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";";
+    std::cout << "amount:" << this->_amount << ";";
+    std::cout << "closed" << std::endl;
 }
 
-Account::Account(int initial_deposit)
+Account::Account(int initial_deposit) : _amount(initial_deposit), 
+	_nbDeposits(0), _nbWithdrawals(0)
 {
-    this->_amount = initial_deposit;
-    this->_nbDeposits = 0;
-    this->_nbWithdrawals = 0;
     this->_accountIndex = Account::_nbAccounts;
     Account::_nbAccounts++;
     Account::_totalAmount += this->_amount;
@@ -45,4 +47,31 @@ void    Account::_displayTimestamp(void)
     std::cout << std::setw(2) << std::setfill('0') << time->tm_min;
     std::cout << std::setw(2) << std::setfill('0') << time->tm_sec;
     std::cout <<"] ";
+}
+
+int Account::getNbAccounts(void)
+{
+    return (Account::_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+    return (Account::_totalAmount);
+}
+
+int Account::getNbDeposits(void)
+{
+    return (Account::_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return (Account::_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void)
+{
+    Account::_displayTimestamp();
+    std::cout <<  "accounts:"<<Account::getNbAccounts()<< ";total:"<<Account::getTotalAmount()<<";deposits:"\
+    << Account::getNbDeposits()<<";withdrawals:"<<Account::getNbWithdrawals()<<std::endl;
 }
