0b8e946217a445fb30aba9f0a8041061438ba053
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-18 20:55:47 +0000

    anything

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 17:25:08 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/18 20:51:11 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/18 20:55:19 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int main()
     z2 = newZombie("Rachid");
     z1.announce();
     z2->announce();
+    randomChump("Amhouch");
     delete z2;
     return (0);
 }
--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,8 +6,14 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 17:27:01 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/18 20:22:30 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/18 20:54:32 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+void randomChump(std::string name)
+{
+    Zombie z(name);
+    z.announce();
+}
