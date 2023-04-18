7c339173dd14b676f57690da155daf797a758b57
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-18 20:38:46 +0000

    anything

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 17:26:04 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/04/18 20:38:07 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+}
+
+Zombie::Zombie(std::string name) : name(name)
+{
+}
+
+Zombie::~Zombie()
+{
+    std::cout << this->name << "is dead" << std::endl;
+}
