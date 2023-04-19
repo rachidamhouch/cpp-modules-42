34b7336521198dd30de005aaf75d5a51395192be
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-19 03:26:25 +0000

    anything

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,8 +6,23 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:46:17 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/19 03:06:49 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:25:05 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
+
+HumanB::HumanB(void) : name("Default"), weapon(NULL){}
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {}
+HumanB::~HumanB(void) {}
+void HumanB::setWeapon(Weapon &weapon)
+{
+    this->weapon = &weapon;
+}
+void HumanB::attack(void)
+{
+    if (this->weapon)
+        std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+    else
+        std::cout << this->name << " attacks with their " << "(He has no weapon /:)" << std::endl;
+}
--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:46:20 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/18 22:29:30 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:20:25 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class HumanB
         Weapon *weapon;
     public:
         HumanB(void);
-        HumanB(std::string anme);
+        HumanB(std::string nmae);
         ~HumanB();
         void attack(void);
         void setWeapon(Weapon &weapon);
