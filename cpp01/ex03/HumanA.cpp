e8f28a61d10ffe12137a217463b4293d655b057e
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-19 03:18:28 +0000

    anything

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,8 +6,15 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:46:11 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/19 03:07:04 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:17:31 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
+
+HumanA::HumanA(std::string name ,Weapon &weapon):name(name) , weapon(weapon) {}
+HumanA::~HumanA(){}
+void HumanA::attack(void)
+{
+    std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
+}
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:46:23 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/18 21:55:40 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:18:22 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 int main()
 {
-    return (0);
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+    return 0;
 }
