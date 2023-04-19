206ca7a5e3a95c5f3ca974ccabf56371ba0d25dd
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-19 03:27:03 +0000

    anything

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:00:43 by ramhouch          #+#    #+#             */
-/*   Updated: 2023/04/18 21:10:23 by ramhouch         ###   ########.fr       */
+/*   Updated: 2023/04/18 21:44:49 by ramhouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Zombie* zombieHorde(int N, std::string name)
 	Zombie *zs;
 
 	zs = new Zombie[N];
-	while (--N >= 0)
+	while (--HumanA.cppN >= 0)
 	{
 		zs[N].set_name(name);
 		zs[N].announce();
