6106d08ea9e549841a7bec4d6154e8e40299d42b
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-13 17:57:56 +0100

    anything

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -9,6 +9,10 @@ class ScalarConverter
 		~ScalarConverter();
 		ScalarConverter& operator=(ScalarConverter const & copy);
         static int check(std::string const & literal);
+        static void prinInt(long double n);
+        static void prinChar(long double n);
+        static void prinFloat(long double n);
+        static void prinDouble(long double n);
     public:
         static void	convert(std::string const & literal);
 
