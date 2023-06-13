66a175e73a92348204de7ebf1af79fd4e58bd36e
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-13 17:43:45 +0100

    anything

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -12,11 +12,28 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter const & copy)
     return (*this);
 }
 
-void ScalarConverter::check(std::string const & literal)
+int ScalarConverter::check(std::string const & literal)
 {
+    int i = 0;
     if ((literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'')
         || (literal.length() == 1 && !isdigit(literal[0])))
-        return;
+        return (0);
+    if (literal[i] == '-' && literal[i] == '+')
+        i++;
+    while (literal[i])
+    {
+        if (!isdigit(literal[i]) && i != (int)literal.length() - 1)
+            throw "char: impossible\n\
+int: impossible\n\
+float: impossible\n\
+double: impossible";
+        i++;
+    }
+    if (literal[i - 1] != 'f' && !isdigit(literal[i - 1]) && literal.length() > 1)
+        throw "char: impossible\n\
+int: impossible\n\
+float: impossible\n\
+double: impossible";
     try
     {
         std::stoi(literal);
@@ -42,12 +59,20 @@ double: impossible";
             }
         }
     }
+    return (1);
 }
 
 void ScalarConverter::convert(std::string const & literal)
 {
-    float n;
-    check(literal);
-    n = std::stof(literal);
+    long double n;
+    if (check(literal))
+        n = std::stold(literal);
+    else
+    {
+        if (literal.length() == 1)
+            n = static_cast<int>(literal[0]);
+        else
+            n = static_cast<int>(literal[1]);
+    }
     std::cout << n << std::endl;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -8,7 +8,7 @@ class ScalarConverter
 		ScalarConverter(ScalarConverter const &copy);
 		~ScalarConverter();
 		ScalarConverter& operator=(ScalarConverter const & copy);
-        static void check(std::string const & literal);
+        static int check(std::string const & literal);
     public:
         static void	convert(std::string const & literal);
 
