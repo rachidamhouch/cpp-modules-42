4256cf01f3e82c95f370c26b964cc899504e0286
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-18 14:16:09 +0100

    anything

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -5,8 +5,6 @@
 template <class T, class Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
-    private:
- 
     public:
         MutantStack(){}
         MutantStack(MutantStack<T> const &copy) : std::stack<T, Container>(copy) {};
@@ -18,17 +16,11 @@ class MutantStack : public std::stack<T, Container>
 		}
         ~MutantStack(){}
 		typedef typename Container::iterator	iterator;
-		typedef typename Container::const_iterator	c_iterator;
-		typedef typename Container::reverse_iterator	r_iterator;
-		typedef typename Container::const_reverse_iterator	c_r_iterator;
+		typedef typename Container::const_iterator	const_iterator;
 
-		iterator		begin(void) {return this->c.begin();}
-		iterator		end(void) {return this->c.end();}
-		c_iterator	    begin(void) const {return this->c.begin();}
-		c_iterator		end(void) const {return this->c.end();}
-		r_iterator		rbegin(void) {return this->c.rbegin();}
-		r_iterator		rend(void) {return this->c.rend();}
-		c_r_iterator	rbegin(void) const {return this->c.rbegin();}
-		c_r_iterator	rend(void) const {return this->c.rend();}
+		iterator			begin(void) {return this->c.begin();}
+		iterator			end(void) {return this->c.end();}
+		const_iterator	    begin(void) const {return this->c.begin();}
+		const_iterator		end(void) const {return this->c.end();}
 };
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,16 +1,17 @@
 #include "MutantStack.hpp"
-#include <vector>
+
 int main()
 {
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    //std::cout << mstack.top() << std::endl;
-    //mstack.pop();
-    //std::cout << mstack.size() << std::endl;
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
+    std::cout << "==================" << std::endl;
     MutantStack<int> mstack1 = mstack;
     MutantStack<int>::iterator it = mstack1.begin();
     MutantStack<int>::iterator ite = mstack1.end();
