33f6205fa507a7ab0468ba836dfc2de8e1c306e3
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-18 12:10:54 +0100

    anything

---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    std::cout << mstack.size() << std::endl;
+    return (0);
+}
