a062f1f3a77715794698ce100a04333bf1261c06
rachidamhouch <rachid.amhouch@gmail.com>
2023-07-20 17:22:15 +0100

    anything

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <ctime>
 
 bool is_valid(char *str)
 {
@@ -16,9 +17,88 @@ bool is_valid(char *str)
     return true;
 }
 
+std::vector<int> insertVec(std::vector<int> &Tab)
+{
+    unsigned long i = 0, tmp;
+
+    while (i < Tab.size() - 1)
+    {
+        if (Tab[i] > Tab[i + 1])
+        {
+            tmp = Tab[i];
+            Tab[i] = Tab[i + 1];
+            Tab[i + 1] = tmp;
+            i = -1;
+        }
+        i++;
+    }
+    return (Tab);
+}
+
+std::vector<int> mergeVec(std::vector<int> &Tab)
+{
+    unsigned long i = 0, n = 0, t = 0;
+    std::vector<int> left, right;
+
+    if (Tab.size() > 100)
+    {
+        while (i < Tab.size() / 2)
+        {
+            left.push_back(Tab[i]);
+            i++;
+        }
+        while (i < Tab.size())
+        {
+            right.push_back(Tab[i]);
+            i++;
+        }
+        left = mergeVec(left);
+        right = mergeVec(right);
+    }
+    else
+        return (insertVec(Tab));
+    i = 0;
+    while(i < left.size() && n < right.size())
+    {
+        if (left[i] < right[n])
+        {
+            Tab[t] = left[i];
+            i++;
+            t++;
+        }
+        else
+        {
+            Tab[t] = right[n];
+            n++;
+            t++;
+        }
+    }
+    while(i < left.size())
+    {
+        Tab[t] = left[i];
+        i++;
+        t++;
+    }
+    while(n < right.size())
+    {
+        Tab[t] = right[n];
+        n++;
+        t++;
+    }
+    return (Tab);
+}
+
+float mergeInserVec(std::vector<int> &Tab)
+{
+    std::clock_t start = std::clock();
+    mergeVec(Tab);
+    std::clock_t end = std::clock();
+    return (static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000.0);
+}
+
 int main(int ac, char **av)
 {
-    int i = 1;
+    unsigned long int i = 1;
     std::vector<int> Vec;
     std::deque<int> Deq;
 
@@ -27,7 +107,7 @@ int main(int ac, char **av)
         std::cerr << "Usage: ./PmergeMe Numbers" << std::endl;
         exit (1);
     }
-    while (i < ac)
+    while (i < (unsigned long)ac)
     {
         if (!is_valid(av[i]))
         {
@@ -35,7 +115,14 @@ int main(int ac, char **av)
             exit (1);
         }
         Vec.push_back(atoi(av[i]));
-        Deq.push_back(atoi(av[i]));
+        (void)(Deq);
+        i++;
+    }
+    mergeInserVec(Vec);
+    i = 0;
+    while (i < Vec.size())
+    {
+        std::cout << Vec[i] << std::endl;
         i++;
     }
     return (0);
